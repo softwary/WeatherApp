5eeb91116118f454c2cf35ae8917dbd029b88a1f
Dominique Adevai <dominiqueadevai@gmail.com>
2018-05-10 20:08:57 +0200

    Just need to get the API to work. :)

--- app/src/main/java/ait/hu/weatherapp/MainActivity.java
+++ app/src/main/java/ait/hu/weatherapp/MainActivity.java
@@ -21,23 +21,16 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, CityDialog.CityHandler {
 
     private CityRecyclerAdapter cityRecyclerAdapter;
-
+    private DrawerLayout activity_nav_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        activity_nav_main = findViewById(R.id.drawer_layout);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -59,51 +52,26 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.nav_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.btnAddCity) {
+            showNewCityDialog();
+        } else if (id == R.id.btnAbout) {
+            Snackbar.make(activity_nav_main, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void showNewCityDialog() {
+        new CityDialog().show(getSupportFragmentManager(), "CityCreateDialog");
     }
 
     @Override
@@ -122,11 +90,5 @@ public class MainActivity extends AppCompatActivity
                 });
             }
         }.start();
-    }
-
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
     }
 }
